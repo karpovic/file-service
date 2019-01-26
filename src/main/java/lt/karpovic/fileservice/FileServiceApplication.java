93be7dacbd2c672ac731af3fc41c70a520eb1d5d
Ruslan Karpovic <karpovic.ruslan@gmail.com>
2019-01-26 10:17:06 +0200

    initialize new spring boot project

---
+++ src/main/java/lt/karpovic/fileservice/FileServiceApplication.java
@@ -0,0 +1,14 @@
+package lt.karpovic.fileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileServiceApplication.class, args);
+	}
+
+}
+
